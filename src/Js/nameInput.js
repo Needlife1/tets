1fb7a330370c75262b1cd2b749579d2b5329c808
Visotsky Maxim <needlife124@gmail.com>
2023-09-20 11:44:02 +0300

    slider

--- src/Js/nameInput.js
+++ src/Js/nameInput.js
@@ -1,21 +1,21 @@
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-var _ = require("lodash");
-export const nameInput = document.addEventListener("DOMContentLoaded", () => {
-  const nameInput = document.querySelector("[data-name-input]");
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var _ = require('lodash');
+export const nameInput = document.addEventListener('DOMContentLoaded', () => {
+  const nameInput = document.querySelector('[data-name-input]');
 
-  nameInput.addEventListener("input", onNameinput);
+  nameInput.addEventListener('input', onNameinput);
 
   function onNameinput(e) {
     const input = e.target;
     const inputNameValue = getInputNameValue(input);
 
     if (!inputNameValue) {
-      input.value = "";
+      input.value = '';
     }
   }
 
   function getInputNameValue(input) {
-    const sanitizedValue = input.value.replace(/[^a-zA-Zа-яА-ЯїєіґҐёЁ']/g, "");
+    const sanitizedValue = input.value.replace(/[^a-zA-Zа-яА-ЯїєіґҐёЁ']/g, '');
     if (input.value !== sanitizedValue) {
       input.value = sanitizedValue;
     }
@@ -23,12 +23,12 @@ export const nameInput = document.addEventListener("DOMContentLoaded", () => {
   }
 
   nameInput.addEventListener(
-    "keydown",
-    _.throttle((e) => {
-      if (e.key >= "0" && e.key <= "9") {
-        Notify.failure("You need to enter letters!!!");
+    'keydown',
+    _.throttle(e => {
+      if (e.key >= '0' && e.key <= '9') {
+        Notify.failure('You need to enter letters!!!');
         e.preventDefault();
       }
-    }, 1500)
+    }, 1500),
   );
 });
--- src/Js/phoneMask.js
+++ src/Js/phoneMask.js
@@ -1,13 +1,13 @@
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-var _ = require("lodash");
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var _ = require('lodash');
 
-export const phoneMask = document.addEventListener("DOMContentLoaded", () => {
-  const telInput = document.querySelector("[data-tel-input]");
+export const phoneMask = document.addEventListener('DOMContentLoaded', () => {
+  const telInput = document.querySelector('[data-tel-input]');
 
-  telInput.addEventListener("input", onTelInput);
+  telInput.addEventListener('input', onTelInput);
 
   const handleInvalidInput = _.throttle(function handleInvalidInputThrottled() {
-    Notify.failure("Please enter valid input.");
+    Notify.failure('Please enter valid input.');
   }, 1500);
 
   function onTelInput(e) {
@@ -16,17 +16,17 @@ export const phoneMask = document.addEventListener("DOMContentLoaded", () => {
 
     if (!inputNumbersValue) {
       handleInvalidInput();
-      return (input.value = "");
+      return (input.value = '');
     }
 
-    if (inputNumbersValue[0] === "0") {
-      input.value = "+38" + inputNumbersValue;
+    if (inputNumbersValue[0] === '0') {
+      input.value = '+38' + inputNumbersValue;
     } else {
-      input.value = "+" + inputNumbersValue.substring(0, 16);
+      input.value = '+' + inputNumbersValue.substring(0, 16);
     }
   }
 
   function gatInputNumbersValue(input) {
-    return input.value.replace(/\D/g, "");
+    return input.value.replace(/\D/g, '');
   }
 });
